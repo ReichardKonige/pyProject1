b5c02186853386578a31e5ffa701be98c81b9cf7
ReichardKonige <31149763+ReichardKonige@users.noreply.github.com>
2019-11-21 02:27:24 -0600

    Master Fire

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 myList = ["apple", "banana", "cherry"]
-myList.append("fire")
+myList.append("Fire")
 print(myList)
 myList.insert(1, "test")
 myList.append("plum")
 print(myList)
-print(myList.index("fire"))
+print(myList.index("Fire"))
 print(myList.index("test"))
