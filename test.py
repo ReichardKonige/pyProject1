481c92d11e3d5c178d5d8f9f366ffaaf9f545239
ReichardKonige <31149763+ReichardKonige@users.noreply.github.com>
2019-11-21 01:45:24 -0600

    Changed pear to plum

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ myList = ["apple", "banana", "cherry"]
 myList.append("orange")
 print(myList)
 myList.insert(1, "test")
-myList.append("pear")
+myList.append("plum")
 print(myList)
 print(myList.index("orange"))
 print(myList.index("test"))
